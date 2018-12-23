013900745216c500af6ddb6f31b303b49242bf99
kvlknctk <kvlknctk@gmail.com>
2018-12-23 13:49:38 +0300

    Bootstarp input required feature added. Ad image size notify.

--- resources/views/adversite/adversite.blade.php
+++ resources/views/adversite/adversite.blade.php
@@ -24,10 +24,10 @@
 
     <hr>
     {{--yükleme alanı--}}
-    <h3>Not: Genişlik : X / Yükseklik : Y</h3>
+    <h3>Not: Genişlik : 360px / Yükseklik : 100px</h3>
     <form action="{{route('reklam.store')}}" method="post" enctype="multipart/form-data">
         @csrf
-        <input type="text" name="title" placeholder="Başlık" class="form-control col-md-6">
+        <input type="text" name="title" placeholder="Başlık" class="form-control col-md-6" required>
         <input type="file" name="reklam" class="form-control col-md-6">
         <button type="submit" class="btn btn-success">Yükle</button>
     </form>
