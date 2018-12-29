41dc12e2faa691915ff6fd90e9a370763f56a5dd
kvlknctk <iletisim@volkancatak.com.tr>
2018-12-29 17:11:48 +0300

    git worker problemi giderildi.

--- resources/views/front/anket.blade.php
+++ resources/views/front/anket.blade.php
@@ -3,7 +3,7 @@
 @section('anket')
     <div class="card mb-4 shadow-sm">
         <div class="card-header">
-            <h4 class="my-0 font-weight-normal">Iğdır / asdf
+            <h4 class="my-0 font-weight-normal">Iğdır / {{@$masterPoll->state->title}}
                 <br>
                 {{--@if($switch) <br> Oy kullandınız @endif--}}
             </h4>
