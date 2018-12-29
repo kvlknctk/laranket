4b658bc32927215f997af8ce0a8b3de023ede9ab
kvlknctk <iletisim@volkancatak.com.tr>
2018-12-29 14:27:14 +0300

    yeni node sürümünde browsersync bu şekilde olmak zorunda.

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,5 +16,7 @@ mix.js('resources/js/app.js', 'public/js')
 
 mix.browserSync({
     proxy: 'cihat.local',
+    open: true,
+    files: ['app','resources', 'public', 'config'],
     notify: false
 });
